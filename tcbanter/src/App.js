9034b3991f5755329b31f2fe0f907f0a63662d6d
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2021-03-14 20:41:08 +0000

    dynamic background

--- tcbanter/src/App.js
+++ tcbanter/src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Header from "./Components/Header";
 import Wave from "./Components/Wave";
 import Trinder from "./Components/Trinder/Trinder";
+import {useState} from "react";
 
 function App() {
+  const [pageRed, setpageRed] = useState(true)
   return (
     <div className="App">
-      <div className="bgColour">
+      <div className={pageRed ? 'bgRed' : 'bgBlue'}>
         <Header />
         <Wave />
+        
         <Trinder />
       </div>
     </div>
